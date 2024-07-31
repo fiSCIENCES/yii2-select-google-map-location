b2577177f6d309e7fed5e831255ee7e44d3f0e0a
andrasatriapratama <andrasatriapratama@gmail.com>
2024-07-31 17:09:46 +0700

    update - update AdvancedMarkerElement

--- js/select-google-map-location.js
+++ js/select-google-map-location.js
@@ -26,7 +26,7 @@
 
         function initialization() {
             async function initMap() {
-                const { Map } = await google.maps.importLibrary("maps");
+                const { Map } = await window.google.maps.importLibrary("maps");
                 const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
                 var mapOptions = {
