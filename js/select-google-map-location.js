5e64ee2629b6171dc41e9db4c9e0756debd481c6
Martin Pineault <martin.pineault@fisciences.com>
2024-08-17 07:48:07 -0400

    Add files via upload

--- js/select-google-map-location.js
+++ js/select-google-map-location.js
@@ -9,8 +9,16 @@
  * @param {Number} options.defaultLatitude Default latitude
  * @param {Number} options.defaultLongitude Default longitude
  * @param {String|jQuery|HTMLInputElement} options.address Address input selector
+ * @param {String|jQuery|HTMLInputElement} options.name Name result from location
+ * @param {String|jQuery|HTMLInputElement} options.addressStreetNumber Street number result from location
+ * @param {String|jQuery|HTMLInputElement} options.addressRoute Route result from location
+ * @param {String|jQuery|HTMLInputElement} options.addressLocality City result from location
+ * @param {String|jQuery|HTMLInputElement} options.addressAdmAreaLevel2 Region result from location
+ * @param {String|jQuery|HTMLInputElement} options.addressAdmAreaLevel1 Province/State result from location
+ * @param {String|jQuery|HTMLInputElement} options.addressPostalCode Postal code result from location
+ * @param {String|jQuery|HTMLInputElement} options.addressCountry Country result from location
  * @param {String|jQuery|HTMLInputElement} options.latitude Latitude input selector
- * @param {String|jQuery|HTMLInputElement} options.latitude Longitude input selector
+ * @param {String|jQuery|HTMLInputElement} options.longitude Longitude input selector
  * @param {Boolean} options.hideMarker Hide\show marker to selected location
  * @param {Function|undefined} options.onLoadMap Callback function to render map
  * @param {String|undefined} options.addressNotFound Description for not found address error
@@ -22,8 +30,8 @@
 
         $(document).ready(function() {
             var mapOptions = {
-                center: new google.maps.LatLng(options.defaultLatitude || 55.997778, options.defaultLongitude || 37.190278),
-                zoom: options.defaultZoom || 12,
+                center: new google.maps.LatLng(options.defaultLatitude || 46.829853, options.defaultLongitude || -71.254028),
+                zoom: 12,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 panControl: true
             };
@@ -64,11 +72,53 @@
                             }
                             selectLocation(results[0]);
                         }
-
                         return false;
                     }
                 );
             };
+
+            /**
+             * Get Address components from Google Map to return
+             *
+             * @param {Object} results JSON
+             */
+            var getComponents = function(results) {
+                $(options.name).val(results.name);
+                $(options.name).trigger('change');
+                for (let i = 0; i < results.address_components.length; i++) {
+                    for (let j = 0; j < results.address_components[i].types.length; j++) {
+                        if (results.address_components[i].types[j] == "street_number") {
+                            $(options.addressStreetNumber).val(results.address_components[i].long_name);
+                            $(options.addressStreetNumber).trigger('change');
+                        }
+                        if (results.address_components[i].types[j] === "route") {
+                            $(options.addressRoute).val(results.address_components[i].long_name);
+                            $(options.addressRoute).trigger('change');
+                        }
+                        // sublocality_level_1 (i.e.: Desjardins)
+                        if (results.address_components[i].types[j] === "locality") {
+                            $(options.addressLocality).val(results.address_components[i].long_name);
+                            $(options.addressLocality).trigger('change');
+                        }
+                        if (results.address_components[i].types[j] === "administrative_area_level_2") {
+                            $(options.addressAdmAreaLevel2).val(results.address_components[i].long_name);
+                            $(options.addressAdmAreaLevel2).trigger('change');
+                        }
+                        if (results.address_components[i].types[j] === "administrative_area_level_1") {
+                            $(options.addressAdmAreaLevel1).val(results.address_components[i].long_name);
+                            $(options.addressAdmAreaLevel1).trigger('change');
+                        }
+                        if (results.address_components[i].types[j] === "country") {
+                            $(options.addressCountry).val(results.address_components[i].long_name);
+                            $(options.addressCountry).trigger('change');
+                        }
+                        if (results.address_components[i].types[j] === "postal_code") {
+                            $(options.addressPostalCode).val(results.address_components[i].long_name);
+                            $(options.addressPostalCode).trigger('change');
+                        }
+                    }
+                }
+            }
 
             /**
              * Create marker into map
@@ -125,6 +175,7 @@
                 if (!item.geometry) {
                     return;
                 }
+                getComponents(item);
                 var bounds = item.geometry.viewport ? item.geometry.viewport : item.geometry.bounds;
                 var center = null;
                 if (bounds) {
